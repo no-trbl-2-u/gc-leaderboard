3035dd39897d75ae734275d6d5178a3c87658cb8
unknown <no.trbl.2.u@gmail.com>
2019-01-06 00:00:37 -0500

    Added New Tournament Text

--- src/Components/TournamentText.tsx
+++ src/Components/TournamentText.tsx
@@ -6,16 +6,13 @@ export default function TournamentText () {
         <div className="TournamentText">
           <h1>Groove Catcher Contests!</h1>
           <h1>$400 in Prizes</h1>
+          
           <div>Top Prize - Synchrony music-reactive LED kit ($150 value) & Groove Catcher T-shirt.
           <div>2nd & 3rd places - Groove Catcher T-shirt.</div>
           </div>
 
-          <div className="two-ways">Two ways to win</div>
 
-          <h2>Leaderboard contest - 7PM Saturday (5pm Cutoff)</h2>
-          The top 10 players during MAGfest will compete Saturday @ 7pm in a two-round tournament.
-          <div>Follow your ranking at <span className="twitter"><a href="vizmoo.com/magfest">vizmoo.com/magfest</a></span> and stay updated on twitter <span className="twitter">@GrooveCatcherVR</span>.</div>
-          <h3>Video Sharing Contest</h3>
+          <h2>Video Sharing Contest</h2>
           Post a video of you or a friend playing Groove Catcher and @ us - <span className="twitter">@GrooveCatcherVR</span>.
           <div>Get as many shares or retweets as you can by Saturday at midnight.</div>
         

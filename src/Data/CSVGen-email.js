c758938d24330b776591f87abf56fe5bee82775d
unknown <no.trbl.2.u@gmail.com>
2019-01-05 23:35:27 -0500

    entry

---
+++ src/Data/CSVGen-email.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const originalEntries = require('./entries.json');
+
+const formatter = originalEntries => {
+  return originalEntries
+    .filter(ea => ea.mailingList === 'true')
+    .filter(ea => ea.email !== '')
+    .map(ea => ({name: ea.name,email: ea.email}
+    ))
+    .map(ea => [ea.name, ea.email])
+    .join('\n')
+};
+const main = () => {
+  const formattedEntry = formatter(originalEntries);
+  console.log(formattedEntry)
+  fs.writeFileSync('emails.txt', formattedEntry);
+  console.log("File properly formatted")
+}
+
+main();

6cafee37d00df76702cf75f514692b1948961bef
unknown <no.trbl.2.u@gmail.com>
2019-05-08 16:53:15 -0400

    Entries - Fillmore

--- src/Components/TournamentText.tsx
+++ src/Components/TournamentText.tsx
@@ -4,10 +4,10 @@ export default function TournamentText () {
     return (
       <section className="TournamentText-container">
         <div className="TournamentText">
-          <h2>Cypher City Spotlight </h2>
+          <h2>Philadelphia Fillmore</h2>
 
           <div>Play and keep your score at the top of the leaderboard</div>
-          <h4>Top leaderboard player will have bragging rights for this year's Cypher City Spotlight</h4>
+          <h4>Top leaderboard player will win themselves a Groove Catcher T-shirt!</h4>
 
           <div>Follow your score: <span className="twitter">vizmoo.com</span></div>
 
